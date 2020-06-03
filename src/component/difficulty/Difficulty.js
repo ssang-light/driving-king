08358454570b0ed503d23a9d51bef5a63eda0724
Younho Choo <younho9.choo@gmail.com>
2020-06-03 21:35:54 +0900

    Create Status bar (#5)
    * Add MenuBar
    
    * Install react-router-dom
    
    * Change name Route to Difficulty
    
    * Add pages contents
    
    * Add MenuBar in App

---
+++ src/component/difficulty/Difficulty.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Difficulty() {
+  return (
+    <div>
+      <h1>지역별 운전 난이도</h1>
+    </div>
+  );
+}
+
+export default Difficulty;
--- src/component/route/Route.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Route() {
-  return;
-}
-
-export default Route;
