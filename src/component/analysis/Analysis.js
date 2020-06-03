cdf0d6e5c4868ca2ba019370da29528c9365bff1
Younho Choo <younho9.choo@gmail.com>
2020-06-03 21:50:34 +0900

    refactor: Change to function components (#7)

--- src/component/analysis/Analysis.js
+++ src/component/analysis/Analysis.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Profile from '../Profile';
 
-const styles = theme => ({
+const styles = (theme) => ({
   root: {
     padding: theme.spacing(3),
     background: '#eeeeee',
@@ -15,44 +16,42 @@ const styles = theme => ({
   },
 });
 
-class Analysis extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <Grid container spacing={3}>
-          <Grid
-            container
-            direction="row"
-            justify="center"
-            alignItems="center"
-            item
-            lg={12}
-            md={12}
-            sm={12}
-            xs={12}
-          >
-            <Paper className={classes.paper}>종합 점수</Paper>
-          </Grid>
-          <Grid item lg={4} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>
-              <div>운전 차량{': SM5'}</div>
-              <img src="https://autoimg.danawa.com/photo/3238/model_360.png" />
-            </Paper>
-          </Grid>
-          <Grid item lg={4} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>지역 숙련도</Paper>
-          </Grid>
-          <Grid item lg={4} md={4} sm={6} xs={12}>
-            <Paper className={classes.paper}>
-              <div>3년 무사고</div>
-              <img width="200" src="asset/images/BestDriverMark.png" />
-            </Paper>
-          </Grid>
+function Analysis({ classes }) {
+  return (
+    <div className={classes.root}>
+      <Profile />
+      <Grid container spacing={3}>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          item
+          lg={12}
+          md={12}
+          sm={12}
+          xs={12}
+        >
+          <Paper className={classes.paper}>종합 점수</Paper>
         </Grid>
-      </div>
-    );
-  }
+        <Grid item lg={4} md={4} sm={6} xs={12}>
+          <Paper className={classes.paper}>
+            <div>운전 차량{': SM5'}</div>
+            <img src="https://autoimg.danawa.com/photo/3238/model_360.png" />
+          </Paper>
+        </Grid>
+        <Grid item lg={4} md={4} sm={6} xs={12}>
+          <Paper className={classes.paper}>지역 숙련도</Paper>
+        </Grid>
+        <Grid item lg={4} md={4} sm={6} xs={12}>
+          <Paper className={classes.paper}>
+            <div>3년 무사고</div>
+            <img width="200" src="asset/images/BestDriverMark.png" />
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default withStyles(styles)(Analysis);
