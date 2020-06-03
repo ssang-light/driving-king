c026b5ac7860a14e053ebeedca756d814605bf99
younho9 <younho9.choo@gmail.com>
2020-06-03 22:15:25 +0900

    refactor: Export MenuBar from App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import MenuBar from './components/MenuBar';
+import Home from './pages/Home';
+import Analysis from './pages/Analysis';
+import Ranking from './pages/Ranking';
+import Difficulty from './pages/Difficulty';
+import Login from './pages/Login';
+import { createBrowserHistory } from 'history';
+import { Router, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var hist = createBrowserHistory();
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/analysis" component={Analysis} />
+      <Route path="/ranking" component={Ranking} />
+      <Route path="/difficulty" component={Difficulty} />
+      <Route path="/login" component={Login} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'),
+);
