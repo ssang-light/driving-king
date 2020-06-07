7115e245fa5df4dab171de8864d0793e8f6f376c
Younho Choo <younho9.choo@gmail.com>
2020-06-07 17:13:14 +0900

    Add difficulty page (#14)

--- src/views/DifficultyPage/DifficultyPage.js
+++ src/views/DifficultyPage/DifficultyPage.js
@@ -1,11 +1,32 @@
-import React, { Fragment } from 'react';
+/* global kakao */
+import React, { Fragment, Component } from 'react';
 import NavBar from 'components/DrivingKing/NavBar';
+import './Map.css';
 
-export default function DifficultyPage() {
-  return (
-    <Fragment>
-      <NavBar />
-      <h1>여기는 운전 난이도</h1>
-    </Fragment>
-  );
+export default class DifficultyPage extends Component {
+  componentDidMount() {
+    const script = document.createElement('script');
+    const API_KEY = process.env.REACT_APP_KAKAOMAP_API_KEY;
+    script.type = 'text/javascript';
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&autoload=false`;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      kakao.maps.load(() => {
+        let el = document.getElementById('map');
+        let map = new kakao.maps.Map(el, {
+          center: new kakao.maps.Coords(523951.25, 1085073.75),
+        });
+      });
+    };
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <NavBar />
+        <div id="map"></div>
+      </Fragment>
+    );
+  }
 }
