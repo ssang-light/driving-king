ab933aae45b561c2f65d953216b3708e01ce49a8
Kyung <kkjhh0207@ajou.ac.kr>
2020-06-07 22:34:56 +0900

    Modify profile

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -32,6 +32,9 @@ import Clearfix from 'components/Clearfix/Clearfix.js';
 import styles2 from 'assets/jss/material-kit-react/views/componentsSections/typographyStyle.js';
 
 import image from 'assets/img/faces/avatar.jpg';
+import f1 from 'assets/img/faces/younho9.jpg';
+import f2 from 'assets/img/faces/marc.jpg';
+import f3 from 'assets/img/faces/kendall.jpg';
 import classNames from 'classnames';
 
 //============================================================
@@ -246,7 +249,7 @@ export default function ProfilePage(props) {
                             <h4>박효성(Parking Park)</h4>
                             <img
                               width="50px"
-                              src={image}
+                              src={f2}
                               alt="..."
                               className={
                                 classes_image.imgRaised +
@@ -269,9 +272,8 @@ export default function ProfilePage(props) {
                         </GridContainer>
                         <GridContainer />
                       </div>
-                      술한잔 했습니다.. 영화가 잘 안될수도 있습니다.. 드라이빙
-                      코스로 강릉찍고 오니 역시 자전거보다는 자동차인것
-                      같습니다.
+                      서울역에서 잠실 넘어간 뒤 팔당댐으로 가는 코스 은근
+                      괜찮습니다. 새벽에 음악틀고 드라이빙하기에 딱입니다.
                       <div>
                         <div>
                           <GridContainer justify="left">
@@ -318,8 +320,8 @@ export default function ProfilePage(props) {
                   tabIcon: Chat,
                   tabContent: (
                     <GridItem>
-                      양양 지나면서 구도로 달리는 것도 좋은 드라이브 코스가 많을
-                      것 같지만 너무 피곤해서 그냥 빠른길로 갔습니다.
+                      팔당댐 근처에 물회 맛집 있습니다. 기가 막힙니다. 댓글
+                      주시면 알려드릴게요
                       <GridItem xs={0} sm={0} md={0}>
                         <img
                           width="1050px"
@@ -374,10 +376,10 @@ export default function ProfilePage(props) {
                             sm={12}
                             className={classes_image.marginLeft}
                           >
-                            <h4>B_Rain</h4>
+                            <h4>younho9</h4>
                             <img
                               width="50px"
-                              src={image}
+                              src={f3}
                               alt="..."
                               className={
                                 classes_image.imgRaised +
@@ -393,16 +395,17 @@ export default function ProfilePage(props) {
                             sm={12}
                             className={classes_image.marginLeft}
                           >
-                            <h4>누적 운전시간 : 152 h</h4>
+                            <h4>누적 운전시간 : 98h</h4>
 
-                            <h4>피드 운전시간 :3h 32m </h4>
+                            <h4>피드 운전시간 :2h 22m </h4>
                           </GridItem>
                         </GridContainer>
                         <GridContainer />
                       </div>
-                      술한잔 했습니다.. 영화가 잘 안될수도 있습니다.. 드라이빙
-                      코스로 강릉찍고 오니 역시 자전거보다는 자동차인것
-                      같습니다.
+                      안양에서 출발해서 서울 양양고속도로 가기 전에 북한강변
+                      쪽으로 빠지는게 빠릅니다. 차도 없고 한적해서 힐링 드라이브
+                      가능합니다. 남이섬 갈때는 고양 고속도로는 별로입니다.
+                      고양으로 꺾으면 미워할고양
                       <div>
                         <div>
                           <GridContainer justify="left">
@@ -449,8 +452,8 @@ export default function ProfilePage(props) {
                   tabIcon: Chat,
                   tabContent: (
                     <GridItem>
-                      양양 지나면서 구도로 달리는 것도 좋은 드라이브 코스가 많을
-                      것 같지만 너무 피곤해서 그냥 빠른길로 갔습니다.
+                      상처를 치료해줄 사람어디 없나 가만히 나두다간 끊임없이
+                      덧나 사랑도 사람도 남이섬에서 만납시다
                       <GridItem xs={0} sm={0} md={0}>
                         <img
                           width="1050px"
@@ -508,7 +511,7 @@ export default function ProfilePage(props) {
                             <h4>B_Rain</h4>
                             <img
                               width="50px"
-                              src={image}
+                              src={f1}
                               alt="..."
                               className={
                                 classes_image.imgRaised +
@@ -531,9 +534,7 @@ export default function ProfilePage(props) {
                         </GridContainer>
                         <GridContainer />
                       </div>
-                      술한잔 했습니다.. 영화가 잘 안될수도 있습니다.. 드라이빙
-                      코스로 강릉찍고 오니 역시 자전거보다는 자동차인것
-                      같습니다.
+                      새차를 180개월 할부로 샀습니다. 오래오래 살아야겠습니다.
                       <div>
                         <div>
                           <GridContainer justify="left">
@@ -544,7 +545,7 @@ export default function ProfilePage(props) {
                               <Button>→</Button>
                               <Button>→</Button>
                               <Button>→</Button>
-                              <Button color="warning">강릉</Button>
+                              <Button color="warning">정동진</Button>
                             </GridItem>
                           </GridContainer>
                         </div>
